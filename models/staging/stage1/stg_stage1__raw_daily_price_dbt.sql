faa3211a74c5ca610d6d090de8f188d92fe6fdaf
Mathilde Le Solliec <mathilde.lesolliec@gmail.com>
2025-03-13 11:54:24 +0000

    triage des données à garder dans stg, et correction de Ile de France en IDF

--- models/staging/stage1/stg_stage1__raw_daily_price_dbt.sql
+++ models/staging/stage1/stg_stage1__raw_daily_price_dbt.sql
@@ -10,45 +10,62 @@ renamed as (
 --Rename + types de data + INICAP pour les majuscules 
     select
         CAST(id AS STRING) AS id_station,
-        CAST(Code_postal AS STRING) AS cp,
         -- je créé une adresse complete pour la carte
-        CONCAT(INITCAP(CAST(Adresse AS STRING)), ' ', CAST(Code_postal AS STRING), ' ', INITCAP(CAST(Ville AS STRING))) AS adresse_complete,
-        geom,
+        CONCAT(INITCAP(CAST(Adresse AS STRING)), ', ', CAST(Code_postal AS STRING), ' ', INITCAP(CAST(Ville AS STRING))) AS adresse_complete,
+        CAST(Code_postal AS STRING) AS cp,
+        CAST(D__partement AS STRING) AS departement,
+        -- je corrige le nom ile de france
+        CASE WHEN R__gion = 'Île-de-France' THEN 'IDF' ELSE CAST(R__gion AS STRING) END AS region,
+        CAST(code_departement AS STRING) AS code_departement,
         ----J'extrait les longitudes et latitudes de geom
+        geom,
         CAST(SPLIT(geom, ',')[SAFE_OFFSET(0)] AS FLOAT64) AS latitude,
         CAST(SPLIT(geom, ',')[SAFE_OFFSET(1)] AS FLOAT64) AS longitude,
-        CAST(Prix_Gazole_mis____jour_le AS DATE) AS gazole_maj,
+        -- info sur les prix
         SAFE_CAST(Prix_Gazole AS FLOAT64) AS gazole_prix,
-        CAST(Prix_SP95_mis____jour_le AS DATE) AS sp95_maj,
         SAFE_CAST(Prix_SP95 AS FLOAT64) AS sp95_prix,
-        CAST(Prix_E85_mis____jour_le AS DATE) AS e85_maj,
         SAFE_CAST(Prix_E85 AS FLOAT64) AS e85_prix,
-        CAST(Prix_GPLc_mis____jour_le AS DATE) AS gplc_maj,
         SAFE_CAST(Prix_GPLc AS FLOAT64) AS gplc_prix,
-        CAST(Prix_E10_mis____jour_le AS DATE) AS e10_prix,
-        CAST(Prix_SP98_mis____jour_le AS DATE) AS sp98_maj,
+        SAFE_CAST(Prix_E10 AS FLOAT64) AS e10_prix,
         SAFE_CAST(Prix_SP98 AS FLOAT64) AS sp98_prix,
-        CAST(D__but_rupture_e10__si_temporaire_ AS DATE) AS e10_rupture_debut,
-        CAST(D__but_rupture_sp98__si_temporaire_ AS DATE) AS sp98_rupture_debut,
-        CAST(D__but_rupture_sp95__si_temporaire_ AS DATE) AS sp95_rupture_debut,
-        CAST(D__but_rupture_e85__si_temporaire_ AS DATE) AS e85_rupture_debut,
-        CAST(D__but_rupture_GPLc__si_temporaire_ AS DATE) AS gplc_rupture_debut,
-        CAST(D__but_rupture_gazole__si_temporaire_ AS DATE) AS gazole_rupture_debut,
-        CAST(Type_rupture_e10 AS STRING) AS e10_rupture_type,
-        CAST(Type_rupture_sp98 AS STRING) AS sp98_rupture_type,
-        CAST(Type_rupture_sp95 AS STRING) AS sp95_rupture_type,
-        CAST(Type_rupture_e85 AS STRING) AS e85_rupture_type,
-        CAST(Type_rupture_GPLc AS STRING) AS gplc_rupture_type,
-        CAST(Type_rupture_gazole AS STRING) AS gazole_rupture_type,
-        SAFE_CAST(Carburants_disponibles AS INT64) AS carburants_disponibles,
-        SAFE_CAST(Carburants_indisponibles AS INT64) AS carburants_indisponibles,
-        SAFE_CAST(Carburants_en_rupture_temporaire AS INT64) AS carburants_rupture_temporaire,
-        SAFE_CAST(Carburants_en_rupture_definitive AS INT64) AS carburants_rupture_definitive,
-        CAST(D__partement AS STRING) AS departement,
-        CAST(code_departement AS STRING) AS code_departement,
-        CAST(R__gion AS STRING) AS region,
-        CAST(code_region AS STRING) AS code_region
-
+        CAST(Prix_Gazole_mis____jour_le AS DATE) AS gazole_maj,
+        CAST(Prix_SP95_mis____jour_le AS DATE) AS sp95_maj,
+        CAST(Prix_E85_mis____jour_le AS DATE) AS e85_maj,
+        CAST(Prix_GPLc_mis____jour_le AS DATE) AS gplc_maj,
+        CAST(Prix_E10_mis____jour_le AS DATE) AS e10_maj,
+        CAST(Prix_SP98_mis____jour_le AS DATE) AS sp98_maj,
+         -- info sur les ruptures
+        CASE 
+            WHEN Type_rupture_e10 = 'temporaire' THEN 'rupture temporaire' 
+            ELSE CAST(Type_rupture_e10 AS STRING) 
+        END AS e10_rupture_type,
+        
+        CASE 
+            WHEN Type_rupture_sp98 = 'temporaire' THEN 'rupture temporaire' 
+            ELSE CAST(Type_rupture_sp98 AS STRING) 
+        END AS sp98_rupture_type,
+        
+        CASE 
+            WHEN Type_rupture_sp95 = 'temporaire' THEN 'rupture temporaire' 
+            ELSE CAST(Type_rupture_sp95 AS STRING) 
+        END AS sp95_rupture_type,
+        
+        CASE 
+            WHEN Type_rupture_e85 = 'temporaire' THEN 'rupture temporaire' 
+            ELSE CAST(Type_rupture_e85 AS STRING) 
+        END AS e85_rupture_type,
+        
+        CASE 
+            WHEN Type_rupture_GPLc = 'temporaire' THEN 'rupture temporaire' 
+            ELSE CAST(Type_rupture_GPLc AS STRING) 
+        END AS gplc_rupture_type,
+        
+        CASE 
+            WHEN Type_rupture_gazole = 'temporaire' THEN 'rupture temporaire' 
+            ELSE CAST(Type_rupture_gazole AS STRING) 
+        END AS gazole_rupture_type
+        
+    
     from source
 )
 
