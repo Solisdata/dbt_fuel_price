8daf0af42c816be84889cae711f6c1bb3abe638f
Mathilde Le Solliec <mathilde.lesolliec@gmail.com>
2025-03-16 16:34:56 +0000

    ajout du calcul de la distance à partir des éléments du  wagon

--- models/staging/stage1/stg_stage1__stations.sql
+++ models/staging/stage1/stg_stage1__stations.sql
@@ -1,40 +1,38 @@
-with 
+with
 
-source as (
+    source as (select * from {{ source("stage1", "stations_dbt") }}),
 
-    select * from {{ source('stage1', 'stations_dbt') }}
+    renamed as (
 
-),
+        select
+            cast(id_station as string) as id_station,
+            nom_station,
+            cast(marque as string) as marque,
+            adresse,
+            code_postal,
+            commune,
+            geo_point,
+            latitude,
+            longitude,
+            gazole,
+            sp95,
+            sp98,
+            gpl,
+            e10,
+            e85,
+            nb_carbu,
+            -- je renomme les routes et autoroutes
+            case
+                when type_route = 'R'
+                then 'Route'
+                when type_route = 'A'
+                then 'Autoroute'
+                else cast(type_route as string)
+            end as type_route
 
-renamed as (
+        from source
 
-    select
-        CAST(id_station AS STRING) AS id_station,
-        nom_station,
-        CAST(marque AS STRING) AS marque,
-        adresse,
-        code_postal,
-        commune,
-        geo_point,
-        latitude,
-        longitude,
-        gazole,
-        sp95,
-        sp98,
-        gpl,
-        e10,
-        e85,
-        nb_carbu,
-        -- je renomme les routes et autoroutes
-        CASE
-            WHEN type_route = 'R' THEN 'Route'
-            WHEN type_route = 'A' THEN 'Autoroute'
-            ELSE CAST(type_route AS STRING)
-        END AS type_routes
+    )
 
-
-    from source
-
-)
-
-select * from renamed
+select *
+from renamed
